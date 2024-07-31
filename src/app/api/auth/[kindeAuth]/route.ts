934dc24c9882b0c13d07ab67c34ea6626f34a0a1
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2024-07-31 20:48:21 +0500

    route.ts argument fix

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,14 +1,15 @@
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextApiRequest, NextApiResponse } from 'next';
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextResponse } from "next/server";
 
-export const GET = (req: NextApiRequest, res: NextApiResponse) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://customcases-uk.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  return handleAuth()(req, res);
-};
+export const GET = (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://customcases-uk.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    
+  
+    return handleAuth()(req, res);
+  };
