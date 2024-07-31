8afc930b7190ffdcd9d99b8dc2af0fbef85f2f7c
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2024-07-31 20:58:05 +0500

    delete cors.ts

--- src/lib/cors.ts
+++
@@ -1,7 +0,0 @@
-import cors, { runMiddleware } from '../../../lib/cors';
-
-export default async function handler(req, res) {
-  await runMiddleware(req, res, cors);
-
-  res.status(200).json({ message: 'Logout successful!' });
-}
