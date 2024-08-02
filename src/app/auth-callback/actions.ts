04f9de9c157638d5c4c99309142f002a347a157b
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2024-08-02 12:20:52 +0500

    next.config

--- src/app/auth-callback/actions.ts
+++ src/app/auth-callback/actions.ts
@@ -1,34 +1,28 @@
-import { NextRequest, NextResponse } from 'next/server'
+'use server'
+
 import { db } from '@/db'
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
 
-export async function GET(req: NextRequest) {
-  try {
-    const { getUser } = getKindeServerSession()
-    const user = await getUser()
+export const getAuthStatus = async () => {
+  const { getUser } = getKindeServerSession()
+  const user = await getUser()
 
-    if (!user?.id || !user.email) {
-      return NextResponse.json({ error: 'Invalid user data' }, { status: 400 })
-    }
-
-    const existingUser = await db.user.findFirst({
-      where: { id: user.id },
-    })
-
-    if (!existingUser) {
-      await db.user.create({
-        data: {
-          id: user.id,
-          email: user.email,
-        },
-      })
-    }
-
-    return NextResponse.json({ success: true })
-  } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json({ error: error.message }, { status: 500 })
-    }
-    return NextResponse.json({ error: 'Unknown error' }, { status: 500 })
+  if (!user?.id || !user.email) {
+    throw new Error('Invalid user data')
   }
+
+  const existingUser = await db.user.findFirst({
+    where: { id: user.id },
+  })
+
+  if (!existingUser) {
+    await db.user.create({
+      data: {
+        id: user.id,
+        email: user.email,
+      },
+    })
+  }
+
+  return { success: true }
 }
--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -2,6 +2,7 @@
 
 import { useQuery } from '@tanstack/react-query'
 import { useEffect, useState } from 'react'
+import { getAuthStatus } from './actions'
 import { useRouter } from 'next/navigation'
 import { Loader2 } from 'lucide-react'
 
@@ -14,54 +15,31 @@ const Page = () => {
     if (configurationId) setConfigId(configurationId)
   }, [])
 
-  const { data, isLoading, error } = useQuery({
+  const { data } = useQuery({
     queryKey: ['auth-callback'],
-    queryFn: async () => {
-      const response = await fetch('/api/auth-callback')
-      if (!response.ok) {
-        throw new Error('Network response was not ok')
-      }
-      return response.json()
-    },
+    queryFn: async () => await getAuthStatus(),
     retry: true,
     retryDelay: 500,
   })
 
-  useEffect(() => {
-    if (data?.success) {
-      if (configId) {
-        localStorage.removeItem('configurationId')
-        router.push(`/configure/preview?id=${configId}`)
-      } else {
-        router.push('/')
-      }
+  if (data?.success) {
+    if (configId) {
+      localStorage.removeItem('configurationId')
+      router.push(`/configure/preview?id=${configId}`)
+    } else {
+      router.push('/')
     }
-  }, [data, configId, router])
-
-  if (isLoading) {
-    return (
-      <div className='w-full mt-24 flex justify-center'>
-        <div className='flex flex-col items-center gap-2'>
-          <Loader2 className='h-8 w-8 animate-spin text-zinc-500' />
-          <h3 className='font-semibold text-xl'>Logging you in...</h3>
-          <p>You will be redirected automatically.</p>
-        </div>
-      </div>
-    )
   }
 
-  if (error) {
-    return (
-      <div className='w-full mt-24 flex justify-center'>
-        <div className='flex flex-col items-center gap-2'>
-          <h3 className='font-semibold text-xl'>Error logging you in</h3>
-          <p>{error.message}</p>
-        </div>
+  return (
+    <div className='w-full mt-24 flex justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='h-8 w-8 animate-spin text-zinc-500' />
+        <h3 className='font-semibold text-xl'>Logging you in...</h3>
+        <p>You will be redirected automatically.</p>
       </div>
-    )
-  }
-
-  return null
+    </div>
+  )
 }
 
 export default Page
