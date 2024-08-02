67b3bbf11c2b49d7abeac54f0a5620f1b7593f77
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2024-08-02 11:18:05 +0500

    raw route.ts and vercel.json

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,28 +1,3 @@
-import { NextRequest, NextResponse } from 'next/server';
+import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
 
-export const GET = async (req: NextRequest) => {
-  try {
-    if (req.method === 'OPTIONS') {
-      return new NextResponse(null, {
-        status: 200,
-        headers: {
-          'Access-Control-Allow-Origin': 'https://customcases-uk.vercel.app',
-          'Access-Control-Allow-Methods': 'GET,OPTIONS',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-        }
-      });
-    }
-
-    return new NextResponse('Success', {
-      status: 200,
-      headers: {
-        'Access-Control-Allow-Origin': 'https://customcases-uk.vercel.app',
-        'Access-Control-Allow-Methods': 'GET,OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-      }
-    });
-  } catch (error) {
-    console.error('Internal Server Error:', error as Error);
-    return new NextResponse(`Internal Server Error: ${(error as Error).message}`, { status: 500 });
-  }
-};
+export const GET = handleAuth();
