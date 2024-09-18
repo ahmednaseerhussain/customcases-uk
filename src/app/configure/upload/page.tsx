a71c3748c8fa33b54a43bd203fae5feb8c52c144
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2024-09-18 14:17:34 +0500

    fix cropper ref

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -8,48 +8,27 @@ import { Image, Loader2, MousePointerSquareDashed } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import { useState, useTransition, useRef } from 'react'
 import Dropzone, { FileRejection } from 'react-dropzone'
-import Cropper from 'react-cropper'
+import Cropper, { ReactCropperElement } from "react-cropper";
 import 'cropperjs/dist/cropper.css'
 import { BASE_PRICE, products } from '@/config/products'
 
 const Page = () => {
   const { toast } = useToast()
-  const searchParams = useSearchParams();
-  const productId = searchParams.get('product');
-  const selectedProduct = products.find(product => product.id === Number(productId)) || {
-    isFrame: false,
-    isText: false,
-    isRnd: false,
-    setImageWidth: 0,
-    setImageHeight: 0,
-    rounded: '',
-    top: '',
-    left: '',
-    assetimage: '',
-    assetimageHeight: 0,
-    assetimageWidth: 0,
-    assetimagePosition: '',
-    placeholderText1: '',
-    fontname: '',
-    font1Size: 0,
-    text1Position: '',
-    text1Color: '',
-    placeholderText2: '',
-    fontname2: '',
-    font2Size: 0,
-    text2Position: '',
-    text2PositionLeft: '',
-    text2Color: '',
-    frameX: 0,
-    frameY: 0,
-  };
+  const searchParams = useSearchParams()
+  const productId = searchParams.get('id')
+  // const productIds = searchParams.get('product');
+  const selectedProduct = products.find(product => product.id === Number(productId));
+  if (!selectedProduct) {
+    console.error('Product not found');
+    return null;
+  }
   const [isDragOver, setIsDragOver] = useState<boolean>(false)
   const [uploadProgress, setUploadProgress] = useState<number>(0)
   const [imageToCrop, setImageToCrop] = useState<string | null>(null)
   const [croppedImage, setCroppedImage] = useState<string | null>(null)
   const [isUploading, setIsUploading] = useState<boolean>(false)
   const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false) // State to manage button state
-  const cropperRef = useRef<CropperJS>(null)
+  const cropperRef = useRef<ReactCropperElement>(null)
   const router = useRouter()
 
   const { startUpload } = useUploadThing('imageUploader', {
