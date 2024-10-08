1e48bf6d209552e0f03f9490979d4ed8deab7071
ahmednaseerhussain <ahmednaseerhussain@gmail.com>
2024-10-08 21:37:29 +0500

    fix csr bail out

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     domains: ['utfs.io'],
   },
   experimental: {
-    missingSuspenseWithCSRBailout: true,  // Disable the rule
+    missingSuspenseWithCSRBailout: false,  // Disable the rule
   },
   async headers() {
     return [
